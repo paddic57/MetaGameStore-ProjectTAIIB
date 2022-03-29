b423eb152cca574c10ba7eab4bc76d528b366c08
Mikołaj J <mikolaj.jucha@spiid.pl>
2022-03-29 17:41:07 +0200

    Modele cd

---
+++ MetaGameStore/Models/Models/ProductGame.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Models
+{
+    public class ProductGame
+    {
+        [Key]
+        public int Id_product { get; set; }
+        [Range(0,5)]
+        public int game_type { get; set; }
+        public DateTime premiere { get; set; }
+        public string pegi { get; set; }
+        [Range(0, 4)]
+        public int platform { get; set; }
+        public string game_mode { get; set; }
+    }
+}
