49f3f32cc5ca0e7e21f5f39669c23d6ff07259bb
paddic57 <patryk.koszala@gmail.com>
2022-04-10 16:45:39 +0200

    tests?

---
+++ MetaGameStore/Tests/UnitOfWorkTests.cs
@@ -0,0 +1,39 @@
+﻿using IRepositories;
+using Models;
+using Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    public  class UnitOfWorkTests
+    {
+        private readonly MyContext context;
+
+
+        public UnitOfWorkTests(MyContext context)
+        {
+            this.context = context;
+        }
+
+        public IOrderDetailsRepository orderDetails { get; set; }
+        public IOrderRepository order { get; set; }
+        public IPaymentRepository payment { get; set; }
+        public IProductGameRepository productGame { get; set; }
+        public IProductRepository product { get; set; }
+        public IUserRepository user { get; set; }
+
+        public async Task<int> SaveAsync()
+        {
+            return await context.SaveChangesAsync();
+        }
+        public int Save()
+        {
+            return context.SaveChanges();
+        }
+    }
+}
+
---
+++ MetaGameStore/Tests/UnitTests.cs
@@ -0,0 +1,68 @@
+﻿using BusinessLayer;
+using MockRepository;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests
+{
+    public class UnitTests
+    {
+        public UnitOfWorkTests unitOfWorkTest;
+
+        [Fact]
+        public void orderDetailsGetByIdOrder_TestValid()
+        {
+            var mockOrderDetailsList = new List<OrderDetails>()
+            {
+                new OrderDetails()
+                {
+                    Id = 1,
+                    productCount = 2,
+                    productPrice = 3.99,
+                    idProduct = 1,
+                    idOrder = 1,
+                },
+                new OrderDetails()
+                {
+                    Id = 2,
+                    productCount = 2,
+                    productPrice = 5.99,
+                    idProduct = 2,
+                    idOrder = 2,
+                },
+                new OrderDetails()
+                {
+                    Id = 3,
+                    productCount = 1,
+                    productPrice = 10.99,
+                    idProduct = 3,
+                    idOrder = 1,
+                }
+            };
+            var mockOrderDetailsRepo = new MockOrderDetailsRepository().mockGetByIdOrder(mockOrderDetailsList);
+
+            /*{
+
+                var mockRepository = new MockRepository<Genre>();
+                mockRepository.Setup(r => r.Get(It.IsAny<int>()))
+                    .Returns(new Genre { Name = "Action" });
+
+                testUnitOfWork.GenreRepository = mockRepository.Object;
+
+                BLGenre bLGenre = new BLGenre(testUnitOfWork);
+
+                Genre genre = bLGenre.Get(1);
+
+                Assert.Equal("Action", genre.Name);
+            }*/
+            //BLOrderDetails bLOrderDetails = new BLOrderDetails(unitOfWorkTest);
+            unitOfWorkTest.orderDetails = mockOrderDetailsList.Object;
+
+        }
+    }
+}
